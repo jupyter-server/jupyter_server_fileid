cdd3eb174de348ee4709941cdde946a7099656e8
Zach Sailer <zsailer@apple.com>
2022-09-09 15:45:53 -0700

    bump version

--- jupyter_server_fileid/__init__.py
+++ jupyter_server_fileid/__init__.py
@@ -2,8 +2,8 @@
 from .extension import FileIdExtension
 
 
-version_info = (0, 1, 0, "", "")
-__version__ = ".".join(map(str, version_info[:3])) + "".join(version_info[3:])
+version_info = (0, 2, 0, "", "")
+__version__ = "0.2.0"
 
 
 def _jupyter_server_extension_points():
