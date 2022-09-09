fe7a0be09f722cce8503f3ad42c8baadb3f360b2
Zach Sailer <zsailer@apple.com>
2022-09-09 15:59:04 -0700

    precommit

--- jupyter_server_fileid/__init__.py
+++ jupyter_server_fileid/__init__.py
@@ -1,7 +1,6 @@
 """A Jupyter Server extension providing an implementation of the File ID service."""
 from .extension import FileIdExtension
 
-
 version_info = (0, 2, 0, "", "")
 __version__ = "0.2.0"
 
