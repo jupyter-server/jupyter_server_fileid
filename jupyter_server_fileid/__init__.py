3c0920430659baa9119b73316dc8993b27f00ab6
David L. Qiu <david@qiu.dev>
2022-10-10 23:33:55 +0000

    Bump to 0.3.0

--- jupyter_server_fileid/__init__.py
+++ jupyter_server_fileid/__init__.py
@@ -1,7 +1,7 @@
 """A Jupyter Server extension providing an implementation of the File ID service."""
 from .extension import FileIdExtension
 
-version_info = (0, 2, 0, "", "")
+version_info = (0, 3, 0, "", "")
 __version__ = ".".join(map(str, version_info[:3])) + "".join(version_info[3:])
 
 
