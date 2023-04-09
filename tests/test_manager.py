c2d7e7878486170c29d28abb8fe560f586a4abd6
david qiu <david@qiu.dev>
2023-04-09 10:53:54 -0700

    Fix behavior for OOB move followed by IB move (#63)
    * add tests that capture the edge case
    
    * fix edge case
    
    * remove disjoint move test
    
    * call commit in case ID already exists

--- tests/test_manager.py
+++ tests/test_manager.py
@@ -470,16 +470,38 @@ def test_move_indexed(any_fid_manager, old_path, new_path, fs_helpers):
     assert any_fid_manager.get_path(old_id) == new_path
 
 
-# test for disjoint move handling
-# disjoint move: any out-of-band move that does not preserve stat info
-def test_disjoint_move_indexed(any_fid_manager, old_path, new_path, fs_helpers):
-    old_id = any_fid_manager.index(old_path)
+def test_oob_ib_move(fid_manager, test_path, fs_helpers):
+    """
+    Test an out-of-band move followed by an in-band move.
+    """
+    new_path_1 = "path1"
+    new_path_2 = "path2"
+    id = fid_manager.index(test_path)
 
-    fs_helpers.delete(old_path)
-    fs_helpers.touch(new_path, dir=True)
-    new_id = any_fid_manager.move(old_path, new_path)
+    # out-of-band
+    fs_helpers.move(test_path, new_path_1)
+    # in-band
+    fs_helpers.move(new_path_1, new_path_2)
+    fid_manager.move(new_path_1, new_path_2)
 
-    assert old_id == new_id
+    assert id == fid_manager.get_id(new_path_2)
+
+
+def test_ib_oob_move(fid_manager, test_path, fs_helpers):
+    """
+    Test an in-band move followed by an out-of-band move.
+    """
+    new_path_1 = "path1"
+    new_path_2 = "path2"
+    id = fid_manager.index(test_path)
+
+    # in-band
+    fs_helpers.move(test_path, new_path_1)
+    fid_manager.move(test_path, new_path_1)
+    # out-of-band
+    fs_helpers.move(new_path_1, new_path_2)
+
+    assert id == fid_manager.get_id(new_path_2)
 
 
 def test_move_recursive(
